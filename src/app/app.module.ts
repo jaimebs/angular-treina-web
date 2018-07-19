2ada7b378500d6c2c61d3e0786f39d717f385b3b
Jaime Barbosa <jaime.barbosa@solucaosistemas.net>
2018-07-19 16:57:42 -0300

    Primeiro componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MeuComponenteComponent } from './meu-componente/meu-componente.component';
+import { ListaComponent } from './pokemon/lista/lista.component';
+import { DetalheComponent } from './pokemon/detalhe/detalhe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeuComponenteComponent,
+    ListaComponent,
+    DetalheComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/pokemon/detalhe/detalhe.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detalhe',
+  templateUrl: './detalhe.component.html',
+  styleUrls: ['./detalhe.component.css']
+})
+export class DetalheComponent implements OnInit {
+
+  constructor() { }
+
+  aluno = {
+    dados: {
+      nome: 'Pikachu'
+    }
+  }
+
+  ngOnInit() {
+  }
+
+}
